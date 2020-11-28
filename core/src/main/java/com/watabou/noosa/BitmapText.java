e1d9424313c1ec8b1a6690b8a79d6e368c184aab
XiYou <2923428803@qq.com>
2020-11-28 12:09:48 +0800

    add

--- core/src/main/java/com/watabou/noosa/BitmapText.java
+++ core/src/main/java/com/watabou/noosa/BitmapText.java
@@ -5,6 +5,9 @@
  * Shattered Pixel Dungeon
  * Copyright (C) 2014-2021 Evan Debenham
  *
+ * Virtual Pixel Dungeon
+ * Copyright (C) 2020-2021 AnsdoShip Studio
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
@@ -37,49 +40,79 @@ public class BitmapText extends Component {
 
 	private static final RenderedText SPACE = new RenderedText();
 	private static final RenderedText NEWLINE = new RenderedText();
-	
+
 	protected String text;
 	protected String[] tokens = null;
 	protected ArrayList<RenderedText> words = new ArrayList<>();
+    protected ArrayList<ColorBlock> underlines = new ArrayList<>();
 	protected boolean multiline = false;
 
 	private int size;
 	private float zoom;
+
 	private int color = -1;
-	
-	private int hightlightColor = Window.TITLE_COLOR;
-	private boolean highlightingEnabled = true;
+	private int[] highlightColors = new int[] {
+        0x000000,
+        0x0000AA,
+        0x00AA00,
+        0x00AAAA,
+        0xAA0000,
+        0xAA00AA,
+        0xFFAA00,
+        0xAAAAAA,
+        0x555555,
+        0x5555FF,
+        0x55FF55,
+        0x55FFFF,
+        0xFF5555,
+        0xFF55FF,
+        0xFFFF55,
+        0xFFFFFF,
+        0xDDD605
+	};
+	private int highlightColor = color;
+	private static final String highlightSymbols = "0123456789abcdef";
+	private int highlightColorIndex = -1;
+	private boolean highlightSymbolDetected = false;
+    private boolean rainbow = false;
+	private static final int NORMAL = -1;
+	private static final int BOLD = 0;
+	private static final int ITALIC = 1;
+	private static final int DELETELINE = 2;
+	private static final int UNDERLINE = 3;
+	private static final int RANDOM = 4;
+	private static int wordStyle = NORMAL;
 
 	public static final int LEFT_ALIGN = 1;
 	public static final int CENTER_ALIGN = 2;
 	public static final int RIGHT_ALIGN = 3;
 	private int alignment = LEFT_ALIGN;
     private float spacing;
-	
-	public BitmapText(int size){
+
+	public BitmapText(int size) {
 		this.size = size;
         spacing = 0.5f;
 	}
 
-	public BitmapText(String text, int size){
+	public BitmapText(String text, int size) {
 		this.size = size;
         spacing = 0.5f;
 		text(text);
 	}
-    
-    public BitmapText(String text, int size, float spacing){
+
+    public BitmapText(String text, int size, float spacing) {
         this.size = size;
         this.spacing = spacing;
         text(text);
 	}
 
-	public void text(String text){
+	public void text(String text) {
 		this.text = LanguageFactory.getTranslation(text);
 
 		if (LanguageFactory.getTranslation(text) != null && !LanguageFactory.getTranslation(text).equals("")) {
-			
+
 			tokens = Game.platform.splitforTextBlock(LanguageFactory.getTranslation(text), multiline);
-			
+
 			build();
 		} else {
             tokens = Game.platform.splitforTextBlock("", multiline);
@@ -87,115 +120,166 @@ public class BitmapText extends Component {
 			build();
         }
 	}
-    
-    public void text(String str1, String str2)
-    {
+
+    public void text(String str1, String str2) {
         text = str1 + " " + LanguageFactory.getTranslation(str2);
-        
+
         if (text != null && !text.equals("")) {
-			
+
 			tokens = Game.platform.splitforTextBlock(text, multiline);
-			
+
 			build();
 		} else {
             tokens = Game.platform.splitforTextBlock("", multiline);
 
             build();
         }
-        
+
     }
 
-	public void text(String text, int maxWidth){
+	public void text(String text, int maxWidth) {
 		this.maxWidth = maxWidth;
 		multiline = true;
 		text(text);
 	}
-    
-    public void text(String text, float spacing){
+
+    public void text(String text, float spacing) {
         this.spacing = spacing;
         text(text);
 	}
 
-	public String text(){
+	public String text() {
 		return text;
 	}
-    
-    public float spacing(){return spacing;}
-    
-    public void spacing(float spacing){
+
+    public float spacing() {return spacing;}
+
+    public void spacing(float spacing) {
         this.spacing = spacing;
         text(text);
     }
 
-	public void maxWidth(int maxWidth){
-		if (this.maxWidth != maxWidth){
+	public void maxWidth(int maxWidth) {
+		if (this.maxWidth != maxWidth) {
 			this.maxWidth = maxWidth;
 			multiline = true;
 			text(text);
 		}
 	}
 
-	public int maxWidth(){
+	public int maxWidth() {
 		return maxWidth;
 	}
-	
-	public void size(int size){
+
+	public void size(int size) {
 	    this.size = size;
 		text(text);
 	}
-	
-	public int size(){
+
+	public int size() {
 	    return size;
 	}
 
-	private synchronized void build(){
+	private synchronized void build() {
 		if (tokens == null) return;
-		
+
 		clear();
 		words = new ArrayList<>();
-		boolean highlighting = false;
-		for (String str : tokens){
-//			for (int i = 0; i < tokens.length; i++) {
-//               String str = tokens[i];
-//                String str2 = tokens[i+1];
-//                int textcolor;
-////			if (str.equals("_") && highlightingEnabled){
-////				highlighting = !highlighting;
-//            if (str.equals("§")){
-//                    String result;//最后解析result 转换成整数型颜色值textcolor
-//                    if (str2.equals("[")){
-//                        for (int n = 2; n < 10; n++) {//检测[]内是否合规
-//                            String str3 = tokens[i+n];
-//                            if (str3.equals("]")){
-//                            if (str3.equals("]") && n == 10){}}
-//                        }
-//                    } else {
-//                        //检测到不是§[]格式 返回到单字节颜色代码
-//                        result = str2;
-//                        
-//                    }
-			//} else 
-            if (str.equals("\n")){
+        underlines = new ArrayList<>();
+
+		highlightColorIndex = -1;
+		highlightSymbolDetected = false;
+        rainbow = false;
+
+		for (String str : tokens) {
+
+            if (str.equals("\n")) {
 				words.add(NEWLINE);
-			} else if (str.equals(" ")){
+            } else if (str.equals(" ")) {
 				words.add(SPACE);
+			} else if (str.equals("§")) {
+            	highlightSymbolDetected = true;
 			} else {
-				RenderedText word = new RenderedText(str, size);
-                    //原来的高亮 word.hardlight(颜色值);即可
-				if (highlighting) word.hardlight(hightlightColor);
-				else if (color != -1) word.hardlight(color);
+				if (highlightSymbolDetected) {
+					if (highlightSymbols.contains(String.valueOf(str.charAt(0)))) {
+						highlightColorIndex = highlightSymbols.indexOf(String.valueOf(str.charAt(0)));
+					} else if (String.valueOf(str.charAt(0)).equals("k")) {
+						wordStyle = wordStyle | RANDOM;
+					} else if (String.valueOf(str.charAt(0)).equals("l")) {
+						wordStyle = wordStyle | BOLD;
+					} else if (String.valueOf(str.charAt(0)).equals("m")) {
+						wordStyle = wordStyle | DELETELINE;
+					} else if (String.valueOf(str.charAt(0)).equals("u")) {
+						wordStyle = UNDERLINE;
+					} else if (String.valueOf(str.charAt(0)).equals("o")) {
+						wordStyle = wordStyle | ITALIC;
+					} else if (String.valueOf(str.charAt(0)).equals("p")) {
+						wordStyle = NORMAL;
+					} else if (String.valueOf(str.charAt(0)).equals("r")) {
+                        rainbow = true;
+					} else if (String.valueOf(str.charAt(0)).equals("[")) {
+
+					}
+					str = str.substring(1);
+					highlightSymbolDetected = false;
+				}
+                RenderedText word;
+
+                if (rainbow) {
+                    word = new RenderedText(str, size) {
+                        private float time = 0;
+                        @Override
+                        public void update() {
+                            super.update();
+                            rm = (float)Math.sin(-(time += Game.elapsed) / 1);
+                            gm = (float)Math.sin((time += Game.elapsed) / 2);
+                            bm = (float)Math.sin((time += Game.elapsed) / 3);
+                            //rm = (float)Math.sin(-(time += Game.elapsed/2f));
+                        }};} else {
+                    word = new RenderedText(str, size);
+                }
+				//原来的高亮 word.hardlight(颜色值);即可
+				if (highlightColorIndex != -1) {
+					word.hardlight(highlightColors[highlightColorIndex]);
+				} else if (color != -1) {
+					word.hardlight(color);
+				}
+				if (wordStyle == UNDERLINE) {
+					word.setStyle(RenderedText.UNDERLINE);
+				} else {
+					word.setStyle(RenderedText.NORMAL);
+				}
 				word.scale.set(zoom);
-				
+                ColorBlock Underline;
+                if (word.getStyle() == RenderedText.UNDERLINE) {
+                    if (rainbow) {
+                        Underline = new ColorBlock(1, 2, 0xFFFFFFFF){
+                            private float time = 0;
+                            @Override
+                            public void update() {
+                                super.update();
+                                rm = (float)Math.sin(-(time += Game.elapsed) / 1);
+                                gm = (float)Math.sin((time += Game.elapsed) / 2);
+                                bm = (float)Math.sin((time += Game.elapsed) / 3);
+                            }}; } else {
+                        Underline = new ColorBlock(1, 2, 0xFFFFFFFF);
+                        Underline.color(word.);
+                    }
+                    underlines.add(Underline);
+                    add(Underline);
+                }
+
 				words.add(word);
 				add(word);
-				
+
 				if (height < word.height()) height = word.height();
 			}
 		}
+
 		layout();
 	}
 
-	public synchronized void zoom(float zoom){
+	public synchronized void zoom(float zoom) {
 		this.zoom = zoom;
 		for (RenderedText word : words) {
 			if (word != null) word.scale.set(zoom);
@@ -203,39 +287,27 @@ public class BitmapText extends Component {
 		layout();
 	}
 
-	public synchronized void hardlight(int color){
+	public synchronized void hardlight(int color) {
 		this.color = color;
 		for (RenderedText word : words) {
-			if (word != null) word.hardlight( color );
+			if (word != null) word.hardlight(color);
 		}
 	}
-	
-	public synchronized void resetColor(){
+
+	public synchronized void resetColor() {
 		this.color = -1;
 		for (RenderedText word : words) {
 			if (word != null) word.resetColor();
 		}
 	}
-	
-	public synchronized void alpha(float value){
+
+	public synchronized void alpha(float value) {
 		for (RenderedText word : words) {
-			if (word != null) word.alpha( value );
-		}
-	}
-	
-	public synchronized void setHightlighting(boolean enabled){
-		setHightlighting(enabled, Window.TITLE_COLOR);
-	}
-	
-	public synchronized void setHightlighting(boolean enabled, int color){
-		if (enabled != highlightingEnabled || color != hightlightColor) {
-			hightlightColor = color;
-			highlightingEnabled = enabled;
-			build();
+			if (word != null) word.alpha(value);
 		}
 	}
 
-	public synchronized void invert(){
+	public synchronized void invert() {
 		if (words != null) {
 			for (RenderedText word : words) {
 				if (word != null) {
@@ -250,7 +322,7 @@ public class BitmapText extends Component {
 		}
 	}
 
-	public synchronized void align(int align){
+	public synchronized void align(int align) {
 		alignment = align;
 		layout();
 	}
@@ -268,62 +340,79 @@ public class BitmapText extends Component {
 		lines.add(curLine);
 
 		width = 0;
-		for (RenderedText word : words){
-			if (word == SPACE){
-				x += 1.5f;
-			} else if (word == NEWLINE) {
-				//newline
-				y += height+2f;
-				x = this.x;
-				nLines++;
-				curLine = new ArrayList<>();
-				lines.add(curLine);
-			} else {
-				if (word.height() > height) height = word.height();
+		for (RenderedText word : words) {
+            for (ColorBlock Underline : underlines) {
+                if (word == SPACE) {
+                    x += 1.5f;
+                    if (word.getStyle() == RenderedText.UNDERLINE) {
+                        Underline.size(x, PixelScene.align(0.49f));
+                        Underline.x = x - 1.5f;
+                        Underline.y = y + 1f;
+                    }
+                } else if (word == NEWLINE) {
+                    //newline
+                    y += height + 2f;
+                    x = this.x;
+                    nLines++;
+                    curLine = new ArrayList<>();
+                    lines.add(curLine);
+                    if (word.getStyle() == RenderedText.UNDERLINE) {
+                        Underline.size(x, PixelScene.align(0.49f));
+                        Underline.x = x - 1.5f;
+                        Underline.y = y + 1f;
+                    }
+                } else {
+                    if (word.height() > height) height = word.height();
 
-				if ((x - this.x) + word.width() > maxWidth && !curLine.isEmpty()){
-					y += height+2f;
-					x = this.x;
-					nLines++;
-					curLine = new ArrayList<>();
-					lines.add(curLine);
-				}
+                    if ((x - this.x) + word.width() > maxWidth && !curLine.isEmpty()) {
+                        y += height + 2f;
+                        x = this.x;
+                        nLines++;
+                        curLine = new ArrayList<>();
+                        lines.add(curLine);
+                        if (word.getStyle() == RenderedText.UNDERLINE) {
+                            Underline.size(x, PixelScene.align(0.49f));
+                            Underline.x = x - 1.5f;
+                            Underline.y = y + 1f;
+                        }
+                    }
 
-				word.x = x;
-				word.y = y;
-				PixelScene.align(word);
-				x += word.width();
-				curLine.add(word);
+                    word.x = x;
+                    word.y = y;
+                    PixelScene.align(word);
+                    x += word.width();
+                    curLine.add(word);
 
-				if ((x - this.x) > width) width = (x - this.x);
-				
-				//TODO spacing currently doesn't factor in halfwidth and fullwidth characters
-				//(e.g. Ideographic full stop)
-				x -= spacing;
+                    if ((x - this.x) > width) width = (x - this.x);
 
-			}
-		}
-		this.height = (y - this.y) + height;
+                    //TODO spacing currently doesn't factor in halfwidth and fullwidth characters
+                    //(e.g. Ideographic full stop)
+                    x -= spacing;
 
-		if (alignment != LEFT_ALIGN){
-			for (ArrayList<RenderedText> line : lines){
-				if (line.size() == 0) continue;
-				float lineWidth = line.get(line.size()-1).width() + line.get(line.size()-1).x - this.x;
-				if (alignment == CENTER_ALIGN){
-					for (RenderedText text : line){
-						text.x += (width() - lineWidth)/2f;
-						PixelScene.align(text);
-					}
-				} else if (alignment == RIGHT_ALIGN) {
-					for (RenderedText text : line){
-						text.x += width() - lineWidth;
-						PixelScene.align(text);
-					}
-				}
-			}
-		}
-	}
-    public float baseLine(){
+                }
+            }
+            this.height = (y - this.y) + height;
+
+            if (alignment != LEFT_ALIGN) {
+                for (ArrayList<RenderedText> line : lines) {
+                    if (line.size() == 0) continue;
+                    float lineWidth = line.get(line.size() - 1).width() + line.get(line.size() - 1).x - this.x;
+                    if (alignment == CENTER_ALIGN) {
+                        for (RenderedText text : line) {
+                            text.x += (width() - lineWidth) / 2f;
+                            PixelScene.align(text);
+                        }
+                    } else if (alignment == RIGHT_ALIGN) {
+                        for (RenderedText text : line) {
+                            text.x += width() - lineWidth;
+                            PixelScene.align(text);
+                        }
+                    }
+                }
+            }
+        }
+    }
+    public float baseLine() {
         return height();
     }
 }
